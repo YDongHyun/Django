dbccf959379a8b98745053de13b1adacefdf87a3
YDongHyun <ekdh7456@gamil.com>
2021-09-15 17:25:24 +0900

    2

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from.import views
 
+app_name='pybo' #앱 이름 설정
+
 urlpatterns=[
-    path('',views.index),
-    path('<int:question_id>/',views.detail), #page ex) pybo/2 처럼 출력된다.
+    path('',views.index,name="index" ),#http://localhost:8000/pybo/ URL은 index, http://localhost:8000/pybo/2와 같은 URL에는 detail 이라는 이름을 부여했다.
+    path('<int:question_id>/',views.detail,name='detail'), #page ex) pybo/2 처럼 출력된다. detail 추가
 ]
